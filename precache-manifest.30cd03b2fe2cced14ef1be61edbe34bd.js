f001847df13a4778eaf4fef6fa700c71481f096e
ubermachine <shivamkatoch@outlook.com>
2020-02-07 00:57:24 +0530

    Updates

---
+++ precache-manifest.30cd03b2fe2cced14ef1be61edbe34bd.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "30328a68ab70fd64cd303892a2d422c2",
+    "url": "/react-Calculator/index.html"
+  },
+  {
+    "revision": "0b72998ecbb924cbf0f3",
+    "url": "/react-Calculator/static/css/2.47e06e2e.chunk.css"
+  },
+  {
+    "revision": "4782a84eafb93dcf16b2",
+    "url": "/react-Calculator/static/css/main.2bb9ff78.chunk.css"
+  },
+  {
+    "revision": "0b72998ecbb924cbf0f3",
+    "url": "/react-Calculator/static/js/2.4623c7fa.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/react-Calculator/static/js/2.4623c7fa.chunk.js.LICENSE"
+  },
+  {
+    "revision": "4782a84eafb93dcf16b2",
+    "url": "/react-Calculator/static/js/main.bd573278.chunk.js"
+  },
+  {
+    "revision": "c735798263ac50baa25c",
+    "url": "/react-Calculator/static/js/runtime-main.9924e9d4.js"
+  }
+]);
